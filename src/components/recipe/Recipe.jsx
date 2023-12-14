bdf9aeae9ddd44a017b6692f6dcb254e82d8f920
maramhamed <Maramhamed83@gmail.com>
2023-12-14 16:07:44 +0300

    remove the news div

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -3,38 +3,32 @@ import React from "react";
 
 const Recipe = (props) => {
   return (
-    <div>
-      <div>
-        <div>
-          <div className="col mb-4">
-            <div className="card h-100">
-              <img
-                src={props.photoName}
-                className="card-img-top img-fluid card-img-custom"
-                alt={props.name}
-                style={{ height: "200px", objectFit: "cover" }}
-              />
-              <div className="card-body">
-                <div className="card-content custom-card-content">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col">
-                        <h5 className="card-title">{props.name}</h5>
-                      </div>
-                      <div className="col-auto">
-                        <button className="btn">
-                          <box-icon name="heart"></box-icon>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p className="card-text">{props.description}</p>
+    <div className="col mb-4">
+      <div className="card h-100">
+        <img
+          src={props.photoName}
+          className="card-img-top img-fluid card-img-custom"
+          alt={props.name}
+          style={{ height: "200px", objectFit: "cover" }}
+        />
+        <div className="card-body">
+          <div className="card-content custom-card-content">
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <h5 className="card-title">{props.name}</h5>
                 </div>
-                <div className="flex-grow-5"></div>
+                <div className="col-auto">
+                  <button className="btn">
+                    <box-icon name="heart"></box-icon>
+                  </button>
+                </div>
               </div>
             </div>
+
+            <p className="card-text">{props.description}</p>
           </div>
+          <div className="flex-grow-5"></div>
         </div>
       </div>
     </div>
