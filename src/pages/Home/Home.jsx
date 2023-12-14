4903803a8f62af0f9d4b39c505523c91554b391a
maramhamed <Maramhamed83@gmail.com>
2023-12-14 10:26:22 +0300

    add home comp

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "../../components/search/Search.jsx";
+const Home = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Home;
